efbd932a2998f523ca67373e727018e614f7531c
Soha Pourpirali <pourpirali.soha@gmail.com>
2021-06-18 14:41:45 -0700

    tried some new gglot functions

--- data-viz.R
+++ data-viz.R
@@ -16,3 +16,39 @@ ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
 nrow(mpg)
 ncol(mpg)
 
+# mapping an aesthetics to a variable
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = class))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
+
+# Faceting data
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ class, nrow = 2)
+
+# Bar charts (two below are interchangable)
+
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut))
+
+ggplot(data = diamonds) + 
+  stat_count(mapping = aes(x = cut))
+
+# Bar chart of proportion, not count
+
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, y = stat(prop), group = 1))
+
+
